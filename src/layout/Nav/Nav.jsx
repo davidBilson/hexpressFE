2678b0c9071f5b76c3d51ba0cbe10da26af7dcf9
davidBilson <davidbilson99@gmail.com>
2023-10-13 08:22:18 +0100

    update

---
+++ src/layout/Nav/Nav.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <header>
+        <nav>
+            <div>Logo</div>
+            <ul>
+                <Link></Link>
+                <Link></Link>
+                <Link></Link>
+                <Link></Link>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Nav
