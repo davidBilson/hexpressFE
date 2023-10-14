83d4a724e035e54d9fc52671e40e846336b538c6
davidBilson <davidbilson99@gmail.com>
2023-10-14 01:17:22 +0100

    update

---
+++ src/components/HomePage/HeroSection/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section>
+        <article>
+            <h1>Fastest Interstate Delivery & Easy Pickup</h1>
+            <p>Easy, fast, from point A to point B, seamlessly, within 24 hours.</p>
+            <form>
+                <input type="text" placeholder='Enter Tracking Number'/>
+                <input type="submit" value="Track Package" />
+            </form>
+        </article>
+        <div> </div>
+    </section>
+  )
+}
+
+export default Hero
