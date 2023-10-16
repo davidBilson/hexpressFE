96cf18d80f078787f1a227a7408105d23209a712
davidBilson <davidbilson99@gmail.com>
2023-10-16 15:21:43 +0100

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/HomePage/HeroSection/Hero'
 import ServiceRange from '../components/HomePage/ServiceRange/ServiceRange'
+import Services from '../components/HomePage/ServicesSection/Services'
 
 const Home = () => {
   return (
     <>
         <Hero />
         <ServiceRange />
+        <Services />
     </>
   )
 }
