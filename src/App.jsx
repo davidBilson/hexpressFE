d9b418ef9b5fca3c836de6bc81ae70ab9f294290
davidBilson <davidbilson99@gmail.com>
2023-10-16 23:52:55 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Navbar from './layout/Nav/Nav'
 import Home from './pages/Home'
+import Footer from './layout/Footer/Footer'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home />} ></Route>
       </Routes>
+      <Footer />
     </>
   )
 }
--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -1,27 +1,30 @@
+import Logo from '../../components/Logo/Logo'
 import style from './Footer.module.css'
+import {FaFacebookSquare, FaTwitterSquare} from 'react-icons/fa'
+import { AiFillInstagram,} from 'react-icons/ai'
 
 const Footer = () => {
     return (
-        <footer>
-            <section>
+        <footer className={style.footer}>
+            <section className={style.footerSection}>
                 <div className={style.logo}>
-                    <Logo />
+                    <Logo className={style.footerLogo} />
                 </div>
                 <div className={style.footerIcons}>
-                    F
-                    I
-                    T
+                    <FaFacebookSquare className={style.footerIcon} />
+                    <AiFillInstagram className={style.footerIcon} />
+                    <FaTwitterSquare className={style.footerIcon} />
                 </div>
             </section>
-            <section>
-                <div>
-                    <span>Terms of Service</span>    
-                    <span>Privacy Policy</span>    
-                    <span>Support</span>    
-                    <span>Careers</span>
+            <section className={style.footerSection}>
+                <div className={style.footerLinks}>
+                    <span className={style.footerLink}>Terms of Service</span>
+                    <span className={style.footerLink}>Privacy Policy</span>
+                    <span className={style.footerLink}>Support</span>
+                    <span className={style.footerLink}>Careers</span>
                 </div>
-                <div>
-                    <p>Copyright @2023 Hexpress. All rights reserved.</p>
+                <div className={style.footerCopyright}>
+                    <p className={style.footerText}>Copyright @2023 Hexpress. All rights reserved.</p>
                 </div>
             </section>
         </footer>
