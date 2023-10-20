4dea77c085d12ec0bab975a80291533f10bf9996
davidBilson <davidbilson99@gmail.com>
2023-10-20 16:12:26 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import './reset.css';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
+const clientID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+  <GoogleOAuthProvider clientId={clientID}>
     <React.StrictMode>
       <BrowserRouter>
         <App />
