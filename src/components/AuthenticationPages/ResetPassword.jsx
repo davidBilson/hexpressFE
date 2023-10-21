7d9f51facacedb3e3ca4149bd1201fe80399b502
davidBilson <davidbilson99@gmail.com>
2023-10-21 07:08:57 +0100

    update

--- src/components/AuthenticationPages/ResetPassword.jsx
+++ src/components/AuthenticationPages/ResetPassword.jsx
@@ -6,22 +6,27 @@ const ResetPassword = () => {
 
   return (
     <section className={style.resetPasswordSection}>
-        <h1 className={style.resetPasswordTitle}>Forgot Password?</h1>
-        <p className={style.resetPasswordText}>Don't worry, you are not alone!</p>
-        <form className={style.resetPasswordForm}>
-            <input
-                type="email"
-                name="recovery_email"
-                placeholder='Enter your email address'
-                className={style.resetPasswordInput}
-            />
-            <button type='submit' className={style.resetPasswordButton}>
-                Send recovery link to email
-            </button>
-        </form>
-        <Link to={'/sign-in'} className={style.resetPasswordLink}>Back to Sign In</Link>
-        <div className={style.resetPasswordInfo}>
-            A recovery link would be sent to the email provided. Click on the link, and follow the instructions to create a new password
+        <div className={style.resetPasswordContainer}>
+            <h2 className={style.resetPasswordTitle}>Forgot Password?</h2>
+            <p className={style.resetPasswordText}>Don't worry, you are not alone!</p>
+            <form className={style.resetPasswordForm}>
+            <label className={style.resetPasswordLabel}>
+                <span>Enter your email address</span>
+                <input
+                    type="email"
+                    name="recovery_email"
+                    placeholder='Enter your email address'
+                    className={style.resetPasswordInput}
+                />
+            </label>
+                <button type='submit' className={style.resetPasswordButton}>
+                    Send recovery link to email
+                </button>
+            </form>
+            <Link to={'/sign-in'} className={style.resetPasswordLink}>Back to Sign In</Link>
+            <div className={style.resetPasswordInfo}>
+                A recovery link would be sent to the email provided. Click on the link, and follow the instructions to create a new password
+            </div>
         </div>
     </section>
   )
