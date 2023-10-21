bf34e6939b136d87fa97f645bc378228842419fb
davidBilson <davidbilson99@gmail.com>
2023-10-21 06:52:20 +0100

    update

--- src/components/AuthenticationPages/ResetPassword.jsx
+++ src/components/AuthenticationPages/ResetPassword.jsx
@@ -1,21 +1,30 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import style from './Authentication.module.css'
 
 const ResetPassword = () => {
 
   return (
-    <section>
-        <h1>Forgot Password?</h1>
-        <p>Don't worry, you are not alone!</p>
-        <form>
-            <input type="email" name="recovery_email" placeholder='Enter your email address' />
-            <button type='submit'>Send recovery link to email</button>
+    <section className={style.resetPasswordSection}>
+        <h1 className={style.resetPasswordTitle}>Forgot Password?</h1>
+        <p className={style.resetPasswordText}>Don't worry, you are not alone!</p>
+        <form className={style.resetPasswordForm}>
+            <input
+                type="email"
+                name="recovery_email"
+                placeholder='Enter your email address'
+                className={style.resetPasswordInput}
+            />
+            <button type='submit' className={style.resetPasswordButton}>
+                Send recovery link to email
+            </button>
         </form>
-        <Link to={'/sign-in'}>Back to Sign In</Link>
-        <div>A recovery link would be sent to the email provided. Click on the link, and follow the instructions to create a new password</div>
+        <Link to={'/sign-in'} className={style.resetPasswordLink}>Back to Sign In</Link>
+        <div className={style.resetPasswordInfo}>
+            A recovery link would be sent to the email provided. Click on the link, and follow the instructions to create a new password
+        </div>
     </section>
   )
 }
 
-export default ResetPassword
+export default ResetPassword;
