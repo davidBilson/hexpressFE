8e5a870b19b5d97ffd0234bab058bb4a84264cfd
davidBilson <davidbilson99@gmail.com>
2023-10-25 14:46:16 +0100

    update

--- src/components/AuthenticationPages/Register.jsx
+++ src/components/AuthenticationPages/Register.jsx
@@ -2,8 +2,40 @@ import style from './Register.module.css'
 
 const Register = () => {
   return (
-    <section>
-        <h1>Create an account</h1>
+    <section className={style.loginSection}>
+      <div className={style.loginContainer}>
+        <h2 className={style.loginTitle}>Login to your account</h2>
+        <p className={style.loginText}>Welcome back! Please enter your details</p>
+        <form className={style.loginForm}>
+          <label className={style.loginLabel}>
+            <span>Email Address</span>
+            <input
+              type="email"
+              name="email"
+              placeholder='Enter your email address'
+              className={style.loginInput}
+              autoComplete='off'
+            />
+          </label>
+          <label className={style.loginLabel}>
+            <span>Password</span>
+            <input
+              type="password"
+              name="password"
+              placeholder='************'
+              className={style.loginInput}
+              autoComplete='off'
+            />
+          </label>
+          <Link to={'/reset-password'} className={style.forgotPasswordLink}>Forgot password?</Link>
+          <button type='submit' className={style.loginButton}>
+            Login
+          </button>
+        </form>
+        <p className={style.signUp} >Don't have an account?
+          <Link to={'/sign-up'} className={style.signUpLink}>Sign up</Link>
+        </p>
+      </div>
     </section>
   )
 }
