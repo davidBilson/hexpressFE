0a64c961aa2693e453bce5283806cbae1c0f99c6
davidBilson <davidbilson99@gmail.com>
2023-10-27 05:26:25 +0100

    update

--- src/components/AuthenticationPages/Login.jsx
+++ src/components/AuthenticationPages/Login.jsx
@@ -13,6 +13,24 @@ const Login = () => {
         onSuccess: (codeResponse) => setUser(codeResponse),
         onError: (error) => console.log('Login Failed:', error)
     });
+    useEffect(
+        () => {
+            if (user) {
+                axios
+                    .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
+                        headers: {
+                            Authorization: `Bearer ${user.access_token}`,
+                            Accept: 'application/json'
+                        }
+                    })
+                    .then((res) => {
+                        setProfile(res.data);
+                    })
+                    .catch((err) => console.log(err));
+            }
+        },
+        [ user ]
+    );
   return (
     <section className={style.loginSection}>
       <div className={style.loginContainer}>
