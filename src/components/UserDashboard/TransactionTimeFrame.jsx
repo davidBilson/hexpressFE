92d1986c8415d4f8a244434dd1d1a9d698df00ef
davidBilson <davidbilson99@gmail.com>
2024-02-04 18:52:46 +0100

    added rechart UI to transaction components on dashboard

--- src/components/UserDashboard/TransactionTimeFrame.jsx
+++ src/components/UserDashboard/TransactionTimeFrame.jsx
@@ -1,8 +1,76 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import style from './styles/Transactions.module.css'
 
 const TransactionTimeFrame = () => {
+  const [timeFrame, setTimeFrame] = useState(true);
+
+  const styleMonth = {
+    backgroundColor: timeFrame ? "rgba(0, 18, 117, 0.6)" : "rgba(0, 18, 117, 0.3)",
+    border: "none",
+    padding: "12px",
+    width: "150px",
+    color: timeFrame ? "white" : "black",
+    outline: "none",
+    cursor: "pointer"
+  }
+  const styleYear = {
+    backgroundColor: !timeFrame ? "rgba(0, 18, 117, 0.6)" : "rgba(0, 18, 117, 0.3)",
+    border: "none",
+    padding: "12px",
+    width: "150px",
+    color: !timeFrame ? "white" : "black",
+    outline: "none",
+    cursor: "pointer"
+  }
   return (
-    <div>TransactionTimeFrame</div>
+    <section>
+        <div>
+          <button style={styleMonth} onClick={() => setTimeFrame((prev) => true)}>Monthly Expense</button>
+          <button style={styleYear} onClick={() => setTimeFrame((prev) => false)}>Annual Expense</button>
+        </div>
+
+        {/* Monthly Expense */}
+        {
+          timeFrame &&
+        <article className={style.timeFrameContainer}>
+          <div>
+            <ul>
+              <li>January</li>
+              <li>February</li>
+              <li>March</li>
+              <li>April</li>
+              <li>May</li>
+              <li>June</li>
+              <li>July</li>
+              <li>August</li>
+              <li>September</li>
+              <li>Octobebr</li>
+              <li>November</li>
+              <li>December</li>
+            </ul>
+          </div>
+        </article>
+        }
+        {/* Annual expense */}
+        {
+          !timeFrame &&
+        <article className={style.timeFrameContainer}>
+          <div>
+            <ul>
+              <li>2022</li>
+              <li>2023</li>
+              <li>2024</li>
+              <li>2025</li>
+              <li>2026</li>
+              <li>2027</li>
+              <li>2028</li>
+              <li>2029</li>
+              <li>2030</li>
+            </ul>
+          </div>
+        </article>
+        }
+      </section>
   )
 }
 
--- src/components/UserDashboard/Transactions.jsx
+++ src/components/UserDashboard/Transactions.jsx
@@ -1,6 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import useStore from '../../store/useStore';
 import style from './styles/Transactions.module.css'
+import TransactionTimeFrame from './TransactionTimeFrame';
+import TransactionCharts from './TransactionCharts';
 
 const Transactions = () => {
   const { user, userName } = useStore();
@@ -9,76 +11,11 @@ const Transactions = () => {
     document.title = user ? `Welcome ${userName.split(" ")[0]} - Transaction Logs` : "HEXPRESS | No.1 Trusted Logistics Company"
     }, [user])
 
-  const [timeFrame, setTimeFrame] = useState(true);
-
-  const styleMonth = {
-    backgroundColor: timeFrame ? "rgba(0, 18, 117, 0.6)" : "rgba(0, 18, 117, 0.3)",
-    border: "none",
-    padding: "12px",
-    width: "150px",
-    color: timeFrame ? "white" : "black",
-    outline: "none"
-  }
-  const styleYear = {
-    backgroundColor: !timeFrame ? "rgba(0, 18, 117, 0.6)" : "rgba(0, 18, 117, 0.3)",
-    border: "none",
-    padding: "12px",
-    width: "150px",
-    color: !timeFrame ? "white" : "black",
-    outline: "none"
-  }
-
   return (
-    <>
-      <section>
-        <div>
-          <button style={styleMonth} onClick={() => setTimeFrame((prev) => true)}>Monthly Expense</button>
-          <button style={styleYear} onClick={() => setTimeFrame((prev) => false)}>Annual Expense</button>
-        </div>
-
-        {/* Monthly Expense */}
-        {
-          timeFrame &&
-        <article>
-          <div>
-            <ul>
-              <li>January</li>
-              <li>February</li>
-              <li>March</li>
-              <li>April</li>
-              <li>May</li>
-              <li>June</li>
-              <li>July</li>
-              <li>August</li>
-              <li>September</li>
-              <li>Octobebr</li>
-              <li>November</li>
-              <li>December</li>
-            </ul>
-          </div>
-        </article>
-        }
-        {/* Annual expense */}
-        {
-          !timeFrame &&
-        <article>
-          <div>
-            <ul>
-              <li>2022</li>
-              <li>2023</li>
-              <li>2024</li>
-              <li>2025</li>
-              <li>2026</li>
-              <li>2027</li>
-              <li>2028</li>
-              <li>2029</li>
-              <li>2030</li>
-            </ul>
-          </div>
-        </article>
-        }
-      </section>
-    </>
+    <section className={style.transactionContainer}> 
+      <TransactionTimeFrame />
+      <TransactionCharts />
+    </section>
   )
 }
 
